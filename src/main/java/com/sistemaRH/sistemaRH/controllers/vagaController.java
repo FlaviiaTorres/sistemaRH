da8c5b13de18940c88f8b20240b0b6dec53861e1
Flávia Torres <flaviiadev@gmail.com>
2023-07-30 00:07:23 -0300

    ajustes finais

--- src/main/java/com/sistemaRH/sistemaRH/controllers/vagaController.java
+++ src/main/java/com/sistemaRH/sistemaRH/controllers/vagaController.java
@@ -1,11 +1,11 @@
 package com.sistemaRH.sistemaRH.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -16,7 +16,7 @@ import com.sistemaRH.sistemaRH.repository.VagaRepository;
 
 import jakarta.validation.Valid;
 
-@RestController
+@Controller
 public class vagaController {
 	
 	@Autowired
@@ -24,7 +24,7 @@ public class vagaController {
 	
 	@Autowired
 	private CandidatoRepository cr;
-
+        
 	// CADASTRA VAGA
 	@RequestMapping(value = "/cadastrarVaga", method = RequestMethod.GET)
 	public String form() {
